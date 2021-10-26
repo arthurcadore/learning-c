bddf052805a462dc1eaf6ba54dcc1bf9ced3710e
Cadore - AC <74879952+Cadore-ac@users.noreply.github.com>
2021-10-26 11:21:58 -0300

    first_class_step5

--- first_class_03.c
+++ first_class_03.c
@@ -9,9 +9,8 @@ int main(){  // calling main fuction to code
 
 float media, num1, num2; // creating 3 variables to use in math operation 
 
-scanf("%f", &num1);
-scanf("%f", &num2);   
-
+scanf("%f", &num1); // importing values and including into the variable "num1"
+scanf("%f", &num2); // importing values and including into the variable "num1"
 
 media = (mum1+num2)/2.0; // including math operation into the variable "media"
 
