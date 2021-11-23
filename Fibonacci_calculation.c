77049f284374628a9f9cb1d13c109a65d7d8c82f
Cadore - AC <74879952+Cadore-ac@users.noreply.github.com>
2021-11-23 12:59:08 -0300

    documentation

--- Fibonacci_calculation.c
+++ Fibonacci_calculation.c
@@ -1,9 +1,9 @@
 // First class - Eng. Telecom. 
-// Name: Cadore.AC
+// Name: Cadore-AC
 
 // ---------------------------------------------------
 
-// Code for this expression: F = (N!*2)/(M!+1)
+// Challange: Doing math operations to get fibonacci sequence with "N" series. 
 
 // ---------------------------------------------------
 
@@ -11,34 +11,36 @@
 
     int main(){  
 
+    // Creating variables for inputs, outputs and math operation. 
     int seriesVaule, outputValue, firstValue = 1, secondValue = 1, processSeries = 2;
 
+    // Validation of input series value, it needs to be positive. 
     do{
 
      printf("Input how many series the script will calculate\n");
 
      scanf("%d", &seriesVaule);
+
      printf("\n\nInputed value of = %d series\n\n", seriesVaule);
 
     }while(seriesVaule < 0);
 
+    //doing math operation by value attribution on variables even and odd.
     while(processSeries <= seriesVaule){
 
         if (processSeries % 2 == 0) {
 
             firstValue=firstValue+secondValue;
-
             outputValue=firstValue;
 
         } else {
 
             secondValue=firstValue+secondValue;
-
             outputValue=secondValue;
 
         }
 
-         printf("Serie N°%d - Fibonacci value = %d\n", processSeries, outputValue);
+        printf("Serie N°%d - Fibonacci value = %d\n", processSeries, outputValue);
         processSeries++;
     }
 
