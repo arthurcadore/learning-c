4eed390a57b302fe404786d404b64123d2e6341f
Cadore - AC <74879952+Cadore-ac@users.noreply.github.com>
2021-11-22 22:54:34 -0300

    homework_6

---
+++ homework_06.c
@@ -0,0 +1,34 @@
+// First class - Eng. Telecom. 
+// Name: Cadore.AC
+
+// ---------------------------------------------------
+
+#include <stdio.h>  // including library "stdio"
+
+int main(){  // calling main fuction to code 
+
+    float sizeValue, shapesValue; // creating 3 variables to use in math operation 
+
+    printf("Entre com o tamanho do triângulo\n");
+
+    scanf("%d", &sizeValue); // importing values and including into the variable "sizeValue"
+
+    while(sizeValue >= 2){
+
+        printf("Entre com o número de triângulos\n");
+
+        scanf("%d", &shapesValue); // importing values and including into the variable "num1"
+
+            while(shapesValue >= 1){
+
+                
+
+            }
+
+
+    }
+
+return 0;
+
+}
+
