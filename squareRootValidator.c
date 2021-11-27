03195970c717b126193180476b23bc16d5987f33
Cadore - AC <74879952+Cadore-ac@users.noreply.github.com>
2021-11-27 01:03:38 -0300

    Add: documentation about sintaxes

--- squareRootValidator.c
+++ squareRootValidator.c
@@ -12,12 +12,16 @@
 
 int main() {
 
+    //Creating variables for input data, and math operations. 
+
     int inputValue, seriesTestingValue = 0, sqrtValue = 1;
 
+    //Creating and long double variable for testing decimal places in decimal numbers. 
     long double outputValue; 
 
     printf("\n Please input the number series for validating!\n ");
 
+    //Testing if the number inputed is bigger than 0. 
     do{
         scanf("%d", &inputValue);
         printf("\n The number inputed correspond to = %d\n\n ", inputValue);
@@ -25,27 +29,28 @@ int main() {
 
     }while(inputValue < 0);
 
+    // Creanting an loop function to test all values between 1 and the input.
     do{
 
+        // Doing the math operation to get the square root value of the input. 
         outputValue = (float) sqrt(sqrtValue);
 
+        // testing if the input is integer or not. 
+        if(ceil(outputValue) == floor(outputValue)){
 
-     
-             if(ceil(outputValue) == floor(outputValue)){
-
-                printf("\n-----------------------------------------------------------\n");
+            printf("\n-----------------------------------------------------------\n");
                 
-                printf("\n The sqrt number of %d is integer, and correspond to = %Lf\n", sqrtValue, outputValue);
+            printf("\n The sqrt number of %d is integer, and correspond to = %Lf\n", sqrtValue, outputValue);
 
-                printf("\n-----------------------------------------------------------\n");
+            printf("\n-----------------------------------------------------------\n");
 
-                seriesTestingValue++;
+            seriesTestingValue++;
 
-             }else{
+        }else{
 
-                printf("\n The sqrt number of %d isn't an intiger number, and it value correspond to = %Lf\n", sqrtValue, outputValue);
+            printf("\n The sqrt number of %d isn't an intiger number, and it value correspond to = %Lf\n", sqrtValue, outputValue);
                 
-            };
+        };
 
         sqrtValue++;
 
