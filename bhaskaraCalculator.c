3cb506287e2c23766a24febef51bbdabcb45eb84
Cadore-ac <arthurbarcella.ifsc@gmail.com>
2021-11-28 22:33:00 -0300

    Add: Documentation of all code sintaxes

--- bhaskaraCalculator.c
+++ bhaskaraCalculator.c
@@ -1,30 +1,48 @@
+// Course: Telecom engineering
+// Name: Cadore-AC
+
+// ---------------------------------------------------
+
+// Challange: Do math operation for bhaskara expression
+
+// ---------------------------------------------------
 #include <math.h>
 #include <stdio.h>
 
 int main() {
+    // Creating variables for input, and doing some math operations
     int inputA, inputB, inputC, inputBExponation, deltaValue;
 
+    // Creating an long double value for square root operation
     long double sqrtDeltaValue;
 
+    // Creating float variables for output (need to be float)
     float outputA, outputB, outputA1, outputB1;
 
+    // Inputing value of variable A
     printf("\nPlease input the value of 'A'!\n");
     scanf("%d", &inputA);
     printf("\nThe value inputed of 'A' is = %d\n", inputA);
 
+    // Inputing value of variable B
     printf("\nPlease input the value of 'B'!\n");
     scanf("%d", &inputB);
     printf("\nThe value inputed of 'B' is = %d\n", inputB);
 
+    // Inputing value of variable C
     printf("\nPlease input the value of 'C'!\n");
     scanf("%d", &inputC);
     printf("\nThe value inputed of 'C' is = %d\n", inputC);
 
+    // Doing squared operation for B value
     inputBExponation = (inputB * inputB);
 
+    // Calculating delta value
     deltaValue = (inputBExponation - 4 * inputA * inputC);
 
-    if (deltaValue > 0) {
+    // Validating if Delta is positive, it need to be for be real.
+    if (deltaValue >= 0) {
+        // Calculating the square root of delta.
         sqrtDeltaValue = (float)sqrt(deltaValue);
 
         printf(
@@ -36,10 +54,13 @@ int main() {
         printf("\n The square root value of Delta correspond to = %Lf\n",
                sqrtDeltaValue);
 
+        // Calculating  the first output of bhaskara
         outputA1 = (float)(sqrtDeltaValue - inputB);
 
+        // Calculating  the second output of bhaskara
         outputB1 = (float)-(sqrtDeltaValue + inputB);
 
+        // Printing the outputs of both bhaskara values.
         outputA = (float)outputA1 / (2 * inputA);
         printf(
             "\n-----------------------------------------------------------"
@@ -53,6 +74,7 @@ int main() {
             "\n-----------------------------------------------------------"
             "\n");
 
+        // Printing error in terminal when the value of delta is smaller than 0.
     } else {
         printf(
             "\n-----------------------------------------------------------"
