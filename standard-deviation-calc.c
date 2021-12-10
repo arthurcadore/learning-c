739ebfb8835a1e0671888b4fe0188a93835d24c7
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2021-12-10 10:57:40 -0300

    wip: main code idea

---
+++ standard-deviation-calc.c
@@ -0,0 +1,47 @@
+// Course: Telecom engineering
+// Name: Cadore-AC
+
+// ---------------------------------------------------
+
+// Challange: Make an C function to calculate the standard devitation of two values. 
+// ----------------------------------------------------
+
+#include <stdio.h>
+
+float uCalc (float vet[], int nValue){
+
+
+}
+
+int main() {
+
+    do {
+        printf("\nPlese input the 'N' series value = ");
+
+        scanf("%d", &nValue);
+
+        printf("\nThe number inputed correspond to = %d \n", nValue);
+
+    } while (nValue < 0);
+
+    float vectorValue[5] = {};
+
+    for(i=0;i<=nValue;i++){
+
+        
+
+
+
+    }
+
+
+
+    // Calulating "u" value: 
+
+    uCalc(vectorValue, nValue);
+
+
+
+
+   return 0; 
+}
