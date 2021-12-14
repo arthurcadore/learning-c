9e81b9de2feafe1fde204128842c58b5168b3c19
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2021-12-14 10:55:41 -0300

    wip: vector media main code introduction

---
+++ vector-media-calc.c
@@ -0,0 +1,46 @@
+// Course: Telecom engineering
+// Name: Cadore-AC
+
+// ---------------------------------------------------
+
+// Challange: 
+// ----------------------------------------------------
+
+#include <stdio.h>
+
+int vetcalc (float vetX[], nValue){
+
+
+}
+
+int main() {
+
+    int nValue = 0; 
+    int cout; 
+
+    do {
+        printf("\nPlese input the 'N' series value = ");
+
+        scanf("%d", &nValue);
+
+        printf("\nThe number inputed correspond to = %d \n", nValue);
+
+    } while (nValue < 0);
+
+    count = int vetcalc(x[], nValue)
+
+
+
+    }
+
+
+
+    // Calulating "u" value: 
+
+    uCalc(vectorValue, nValue);
+
+
+
+
+   return 0; 
+}
