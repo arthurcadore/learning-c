32ac401ed336d1245534837b09392b8db425c4ef
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2021-12-19 02:31:48 -0300

    Wip: media vector particular function

--- vector-media-calc.c
+++ vector-media-calc.c
@@ -2,7 +2,6 @@
 // Name: Cadore-AC
 
 // ---------------------------------------------------
-
 // Challange: 
 // ----------------------------------------------------
 
@@ -12,14 +11,28 @@ float media_vetor(int x[], int tamanho){
 
     int i=0;
     
-    float soma=0;
+    float soma=0, soma_harmonica=0;
 
     for(i==0;i<tamanho;i++){
 
-        soma=(float)1/x[i]+soma;
+        if (x[i]==0){
+            
+            return 0;
 
-        printf("valor soma = %f\n", soma);
+        }else{
+
+            soma=(float)1/x[i]+soma;
+
+            printf("valor soma = %f\n", soma);
+
+        };
     };
+
+    soma_harmonica=(float)tamanho/soma;
+
+    printf("Valor Soma Harmonica = %f\n", soma_harmonica);
+
+    //return soma_harmonica;
 
 };
 
@@ -28,9 +41,10 @@ float media_vetor(int x[], int tamanho){
 
 int main(){
 
-    int y[3]={3, 3, 3}, w[3]={5, 9 ,1};
+    int y[3]={3, 3, 3}, w[3]={5, 7 ,1};
 
     media_vetor(y, 3);
+    media_vetor(w, 3);
 
     //printf("retorno %d\n", compara_vetores(y,w,3));
 
