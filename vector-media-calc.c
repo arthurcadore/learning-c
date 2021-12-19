727ad19a43b37020bef46f91ba11e49fbac223b8
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2021-12-19 03:06:39 -0300

    Feat: finished main code function

--- vector-media-calc.c
+++ vector-media-calc.c
@@ -2,7 +2,7 @@
 // Name: Cadore-AC
 
 // ---------------------------------------------------
-// Challange: 
+// Challange: MTM2.2 
 // ----------------------------------------------------
 
 #include <stdio.h>
@@ -23,30 +23,55 @@ float media_vetor(int x[], int tamanho){
 
             soma=(float)1/x[i]+soma;
 
-            printf("valor soma = %f\n", soma);
-
         };
     };
 
     soma_harmonica=(float)tamanho/soma;
 
-    printf("Valor Soma Harmonica = %f\n", soma_harmonica);
-
-    //return soma_harmonica;
+    return soma_harmonica;
 
 };
 
 
-//int compara_vetores(int x[], int y[], int tamanho)
+int compara_vetores(int x[], int y[]){
+
+    if(x[1]==0){
+
+        return -2;
+
+    }else if(y[1]==0){
+
+        return -2;
+
+    }else if(x[1]==y[1]){
+
+        return 0;
+
+    }else if(x[1] > y[1]){
+
+        return 1;
+
+    }else if(x[1] < y[1]){
+
+        return -1;
+    };
+};
 
 int main(){
 
-    int y[3]={3, 3, 3}, w[3]={5, 7 ,1};
+    int output; 
 
-    media_vetor(y, 3);
-    media_vetor(w, 3);
+    int z[3]={16, 56, 8}, w[3]={4, 8, 8}, x[1], y[1];
 
-    //printf("retorno %d\n", compara_vetores(y,w,3));
+    x[1]=media_vetor(z, 3);
+    printf("\n\nValor do primeiro vetor = %d\n", x[1]);
+
+    y[1]=media_vetor(w, 3);
+    printf("Valor do segundo vetor = %d\n\n", y[1]);
+
+    output = compara_vetores(x,y);
+
+    printf("Retorno da comparação entre vetores = %d\n", output);
 
 };
 
