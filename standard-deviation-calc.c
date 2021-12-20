e62afd946a1e0a78e937cb2e13ef4698833d9a5d
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2021-12-20 17:58:53 -0300

    Feat: add sommation fuction code

--- standard-deviation-calc.c
+++ standard-deviation-calc.c
@@ -8,39 +8,39 @@
 
 #include <stdio.h>
 
-float uCalc (float vet[], int nValue){
+int summation(int summationSeries, float input){
+
+    int i; 
+
+    float output = 0, output1; 
+
+    do{
+    
+    printf("\nPlese input the summation series value = ");
+
+    scanf("%d", &summationSeries);
+
+    }while(summationSeries<=0);
+
+    printf("\nPlease input the summation value = ");
+
+    scanf("%f", &input);
+
+    for(i=1;i<=summationSeries;i++){
+
+        output1 =(float) i*input;
+
+        output =(float) output1 + output; 
+
+    }
+
+    printf("\nThe summation value is = %f\n\n", output);
 
 
 }
 
+
 int main() {
-
-    do {
-        printf("\nPlese input the 'N' series value = ");
-
-        scanf("%d", &nValue);
-
-        printf("\nThe number inputed correspond to = %d \n", nValue);
-
-    } while (nValue < 0);
-
-    float vectorValue[5] = {};
-
-    for(i=0;i<=nValue;i++){
-
-        
-
-
-
-    }
-
-
-
-    // Calulating "u" value: 
-
-    uCalc(vectorValue, nValue);
-
-
 
 
    return 0; 
